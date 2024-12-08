d9179f9d0948be7cc62387143f103ced3a943883
imaanick <nickolaytrusov01@gmail.com>
2024-12-08 13:38:22 +0300

    feat: add user check in tasks endpoints

--- app/api/task.py
+++ app/api/task.py
@@ -2,9 +2,12 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 
+from app.adapters.sqlalchemy_db.models import User
+from app.api.depends_stub import Stub
 from app.application.exceptions import MissingTasksError, DataConflictError, TaskNotFoundError
+from app.application.fastapi_users import fastapi_users
 from app.application.models import TaskCreate, TaskResponse, TaskTitleUpdate, TaskUpdate, ReorderRequest
-from app.application.protocols.database import DatabaseGateway
+from app.application.protocols.database import DatabaseGateway, UserDataBaseGateway
 from app.application.task import add_task, delete_task_from_list, get_tasks, update_task_title_by_id, update_task_by_id, \
     tasks_reorder
 
@@ -15,7 +18,10 @@ task_router = APIRouter()
 async def create_task(
         database: Annotated[DatabaseGateway, Depends()],
         task: TaskCreate,
+        user: User = Depends(fastapi_users.current_user(optional=True)),
 ) -> TaskResponse:
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
     new_task = await add_task(task, database)
     return new_task
 
@@ -24,7 +30,11 @@ async def create_task(
 async def delete_task(
         database: Annotated[DatabaseGateway, Depends()],
         task_id: int,
+        user: User = Depends(fastapi_users.current_user(optional=True)),
+
 ) -> dict:
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
     deleted_task_id = await delete_task_from_list(task_id, database)
     if deleted_task_id is None:
         raise HTTPException(status_code=404, detail="Task not found")
@@ -34,9 +44,12 @@ async def delete_task(
 @task_router.get("/", response_model=list[TaskResponse])
 async def read_tasks(
         database: Annotated[DatabaseGateway, Depends()],
+        user: User = Depends(fastapi_users.current_user(optional=True)),
         skip: int = 0,
         limit: int = 10,
 ) -> list[TaskResponse]:
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
     tasks = await get_tasks(skip, limit, database)
     return tasks
 
@@ -46,7 +59,10 @@ async def update_task_title(
         task_id: int,
         task_update: TaskTitleUpdate,
         database: Annotated[DatabaseGateway, Depends()],
+        user: User = Depends(fastapi_users.current_user(optional=True)),
 ) -> TaskResponse:
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
     updated_task = await update_task_title_by_id(task_id, task_update, database)
     if updated_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
@@ -58,7 +74,10 @@ async def update_task(
         task_id: int,
         task_update: TaskUpdate,
         database: Annotated[DatabaseGateway, Depends()],
+        user: User = Depends(fastapi_users.current_user(optional=True)),
 ) -> TaskResponse:
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
     updated_task = await update_task_by_id(task_id, task_update, database)
     if updated_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
@@ -69,7 +88,10 @@ async def update_task(
 async def reorder_tasks(
         reorder_data: ReorderRequest,
         database: Annotated[DatabaseGateway, Depends()],
+        user: User = Depends(fastapi_users.current_user(optional=True)),
 ) -> dict:
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
     try:
         await tasks_reorder(reorder_data, database)
     except MissingTasksError as e:
